6390593f082f624c74bf39fe3fed1e12eb97dbc5
mahmu <mahmu@DESKTOP-AAD639O>
2022-06-16 20:54:13 +0300

    16 Haziran Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,11 +9,22 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
-
-            foreach (var product in productManager.GetByUnitPrice(50,100))
+            ProductTest();
+        }
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
 
             }
         }
